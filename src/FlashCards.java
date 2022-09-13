11f237ed84462ba3cb26eef5f4fbd54518cc3201
LeoGamerMan <mirzajanzadeh.leo@gmail.com>
2022-09-13 10:06:19 +0200

    Flashcards class

--- src/FlashCards.java
+++ src/FlashCards.java
@@ -1,2 +1,9 @@
 public class FlashCards {
+    String question;
+    String answer;
+
+    FlashCards(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
 }
