5a7d4d21107dc2bd3f48a671f2cf38f3b1fa6cab
LeoGamerMan <mirzajanzadeh.leo@gmail.com>
2022-09-13 10:19:26 +0200

    Added getters

--- src/FlashCards.java
+++ src/FlashCards.java
@@ -1,9 +1,17 @@
 public class FlashCards {
-    String question;
-    String answer;
+    private final String question;
+    private final String answer;
 
     FlashCards(String question, String answer) {
         this.question = question;
         this.answer = answer;
+    }
+
+    String getQuestion() {
+        return question;
+    }
+
+    String getAnswer() {
+        return answer;
     }
 }
