d8b53bba68c4f8354a5e7267590ff05de96c9783
steinnatalie <97890750+steinnatalie@users.noreply.github.com>
2022-10-10 11:39:12 +0200

    Update CreateSetMultipleAnswer.java

--- src/Controller/CreateSetMultipleAnswer.java
+++ src/Controller/CreateSetMultipleAnswer.java
@@ -15,20 +15,20 @@
    <Line endX="233.0" endY="36.166664123535156" layoutX="334.0" layoutY="17.0" startX="8.0" startY="36.166664123535156" />
    <TextField fx:id="defTextC" layoutX="342.0" layoutY="19.0" prefHeight="25.0" prefWidth="226.0" text="Deffo" />
    <TextField fx:id="termTextC" layoutX="53.0" layoutY="19.0" prefHeight="25.0" prefWidth="226.0" text="Term" />
-   <Button layoutX="604.0" layoutY="19.0" mnemonicParsing="false" onMouseClicked="#removeCreateListItem" text="X" />
-   <Pane layoutY="89.0" prefHeight="268.0" prefWidth="652.0" style="-fx-background-color: #FBC4BD;" />
+   <Pane layoutY="89.0" prefHeight="268.0" prefWidth="652.0" style="-fx-background-color: #FBC4BD;">
+      <children>
+         <Button layoutX="609.0" layoutY="226.0" mnemonicParsing="false" onMouseClicked="#removeCreateListItem" text="X" />
+      </children>
+   </Pane>
    <Text layoutX="342.0" layoutY="156.0" strokeType="OUTSIDE" strokeWidth="0.0" text="Wrong answear 1" />
    <Line endX="233.0" endY="36.166664123535156" layoutX="334.0" layoutY="96.0" startX="8.0" startY="36.166664123535156" />
    <TextField fx:id="defTextC1" layoutX="342.0" layoutY="98.0" prefHeight="25.0" prefWidth="226.0" text="Deffo" />
-   <Button layoutX="604.0" layoutY="98.0" mnemonicParsing="false" onMouseClicked="#removeCreateListItem" text="X" />
    <Text layoutX="342.0" layoutY="237.0" strokeType="OUTSIDE" strokeWidth="0.0" text="Wrong answear 2" />
    <Line endX="233.0" endY="36.166664123535156" layoutX="334.0" layoutY="177.0" startX="8.0" startY="36.166664123535156" />
    <TextField fx:id="defTextC11" layoutX="342.0" layoutY="179.0" prefHeight="25.0" prefWidth="226.0" text="Deffo" />
-   <Button layoutX="604.0" layoutY="179.0" mnemonicParsing="false" onMouseClicked="#removeCreateListItem" text="X" />
    <Text layoutX="342.0" layoutY="321.0" strokeType="OUTSIDE" strokeWidth="0.0" text="Wrong answear 3" />
    <Line endX="233.0" endY="36.166664123535156" layoutX="334.0" layoutY="261.0" startX="8.0" startY="36.166664123535156" />
    <TextField fx:id="defTextC12" layoutX="342.0" layoutY="263.0" prefHeight="25.0" prefWidth="226.0" text="Deffo" />
-   <Button layoutX="604.0" layoutY="263.0" mnemonicParsing="false" onMouseClicked="#removeCreateListItem" text="X" />
    <Line endX="584.3333129882812" endY="36.16667938232422" layoutX="45.0" layoutY="53.0" startX="-25.0" startY="36.16667938232422" />
    <Line endX="584.3333129882812" endY="36.16667938232422" layoutX="46.0" layoutY="132.0" startX="-25.333343505859375" startY="36.16667175292969" />
    <Line endX="584.3333129882812" endY="36.16667938232422" layoutX="45.0" layoutY="214.0" startX="-21.0" startY="36.16667175292969" />
