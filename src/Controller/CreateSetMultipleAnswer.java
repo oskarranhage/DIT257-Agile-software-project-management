05f327616d7f9e18cf76ab116194bf02f1fadb8b
steinnatalie <97890750+steinnatalie@users.noreply.github.com>
2022-10-10 11:37:07 +0200

    Update CreateSetMultipleAnswer.java

--- src/Controller/CreateSetMultipleAnswer.java
+++ src/Controller/CreateSetMultipleAnswer.java
@@ -1,95 +1,35 @@
-package Controller;
+<?xml version="1.0" encoding="UTF-8"?>
 
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
+<?import javafx.scene.control.Button?>
+<?import javafx.scene.control.TextField?>
+<?import javafx.scene.layout.AnchorPane?>
+<?import javafx.scene.layout.Pane?>
+<?import javafx.scene.shape.Line?>
+<?import javafx.scene.text.Text?>
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-public class CreateSetMultipleAnswer extends AnchorPane {
-
-    FXController controller;
-    String id;
-
-    @FXML private TextField termTextC;
-    @FXML private TextField defText1C;
-    @FXML private TextField defText2C;
-    @FXML private TextField defText3C;
-    @FXML private TextField defText4C;
-
-    /** Constructor for order with fxml */
-    public CreateSetMultipleAnswer(FXController controller, String id) {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("createSetMultipleAnswer.fxml"));
-
-        fxmlLoader.setRoot(this);
-        fxmlLoader.setController(this);
-
-        try {
-            fxmlLoader.load();
-        } catch (IOException exception) {
-            throw new RuntimeException(exception);
-        }
-
-        this.controller = controller;
-        this.id = id;
-    }
-
-    public String getTerm(){
-        return termTextC.getText();
-    }
-
-    public String getDefinition(int n){
-        return switch (n) {
-            case 1 -> defText1C.getText();
-            case 2 -> defText2C.getText();
-            case 3 -> defText3C.getText();
-            case 4 -> defText4C.getText();
-            default -> "";
-        };
-    }
-
-    public List<String> getDefinitions(){
-        List<String> defs = new ArrayList<>();
-        defs.add(defText1C.getText());
-        defs.add(defText2C.getText());
-        defs.add(defText3C.getText());
-        defs.add(defText4C.getText());
-        return defs;
-    }
-
-
-    public void removeCreateListItem(){
-        controller.db.getCreateSingleListItems().removeIf(item -> id.equals(item.id));
-        controller.updateCreateSingleFlowPane(controller.db.getCreateSingleListItems());
-    }
-}
-
-/*
-    public void makeFile()  {
-        try{
-            File mySet = new File ("filename.txt");
-            if (mySet.createNewFile()) {
-                System.out.println("File created: " + mySet.getName());
-            } else {
-                System.out.println("File already exists.");
-            }
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
-    }
-
-    public void writeToFile(String name, String string) {
-        Path path = Path.of("./Sets/"+ name + ".txt");
-        try {
-            Files.writeString(path, string, StandardCharsets.UTF_8);
-        }
-        catch (IOException ex) {
-            System.out.print("Invalid Path");
-        }
-
-    }*/
-
+<fx:root maxHeight="-Infinity" maxWidth="-Infinity" minHeight="-Infinity" minWidth="-Infinity" prefHeight="357.0" prefWidth="652.0" type="AnchorPane" xmlns="http://javafx.com/javafx/17" xmlns:fx="http://javafx.com/fxml/1">
+   <Pane prefHeight="88.0" prefWidth="652.0" style="-fx-background-color: #C8FDDD;" />
+   <Text layoutX="53.0" layoutY="69.0" strokeType="OUTSIDE" strokeWidth="0.0" text="Term" />
+   <Text layoutX="342.0" layoutY="69.0" strokeType="OUTSIDE" strokeWidth="0.0" text="Definition" />
+   <Line endX="233.0" endY="36.166664123535156" layoutX="45.0" layoutY="18.0" startX="8.0" startY="36.166664123535156" />
+   <Line endX="233.0" endY="36.166664123535156" layoutX="334.0" layoutY="17.0" startX="8.0" startY="36.166664123535156" />
+   <TextField fx:id="defTextC" layoutX="342.0" layoutY="19.0" prefHeight="25.0" prefWidth="226.0" text="Deffo" />
+   <TextField fx:id="termTextC" layoutX="53.0" layoutY="19.0" prefHeight="25.0" prefWidth="226.0" text="Term" />
+   <Button layoutX="604.0" layoutY="19.0" mnemonicParsing="false" onMouseClicked="#removeCreateListItem" text="X" />
+   <Pane layoutY="89.0" prefHeight="268.0" prefWidth="652.0" style="-fx-background-color: #FBC4BD;" />
+   <Text layoutX="342.0" layoutY="156.0" strokeType="OUTSIDE" strokeWidth="0.0" text="Wrong answear 1" />
+   <Line endX="233.0" endY="36.166664123535156" layoutX="334.0" layoutY="96.0" startX="8.0" startY="36.166664123535156" />
+   <TextField fx:id="defTextC1" layoutX="342.0" layoutY="98.0" prefHeight="25.0" prefWidth="226.0" text="Deffo" />
+   <Button layoutX="604.0" layoutY="98.0" mnemonicParsing="false" onMouseClicked="#removeCreateListItem" text="X" />
+   <Text layoutX="342.0" layoutY="237.0" strokeType="OUTSIDE" strokeWidth="0.0" text="Wrong answear 2" />
+   <Line endX="233.0" endY="36.166664123535156" layoutX="334.0" layoutY="177.0" startX="8.0" startY="36.166664123535156" />
+   <TextField fx:id="defTextC11" layoutX="342.0" layoutY="179.0" prefHeight="25.0" prefWidth="226.0" text="Deffo" />
+   <Button layoutX="604.0" layoutY="179.0" mnemonicParsing="false" onMouseClicked="#removeCreateListItem" text="X" />
+   <Text layoutX="342.0" layoutY="321.0" strokeType="OUTSIDE" strokeWidth="0.0" text="Wrong answear 3" />
+   <Line endX="233.0" endY="36.166664123535156" layoutX="334.0" layoutY="261.0" startX="8.0" startY="36.166664123535156" />
+   <TextField fx:id="defTextC12" layoutX="342.0" layoutY="263.0" prefHeight="25.0" prefWidth="226.0" text="Deffo" />
+   <Button layoutX="604.0" layoutY="263.0" mnemonicParsing="false" onMouseClicked="#removeCreateListItem" text="X" />
+   <Line endX="584.3333129882812" endY="36.16667938232422" layoutX="45.0" layoutY="53.0" startX="-25.0" startY="36.16667938232422" />
+   <Line endX="584.3333129882812" endY="36.16667938232422" layoutX="46.0" layoutY="132.0" startX="-25.333343505859375" startY="36.16667175292969" />
+   <Line endX="584.3333129882812" endY="36.16667938232422" layoutX="45.0" layoutY="214.0" startX="-21.0" startY="36.16667175292969" />
+</fx:root>
